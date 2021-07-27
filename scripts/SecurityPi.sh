fca32a445b5b6a38335c97b5addd3bb5c71cc70f
GingerCam <cameroncoull@gmail.com>
2021-07-27 11:26:31 +0100

    Initial

--- scripts/SecurityPi.sh
+++ scripts/SecurityPi.sh
@@ -3,7 +3,15 @@
 source /opt/functions.sh
 
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-  network=online
+    network=online
 else
-  network=offline
+    network=offline
+fi
+
+for program in $dependency_file; do
+    command_exists $program
+done
+
+if [[ "$installed" -ne "all" ]]; then
+    install_dependencies
 fi
