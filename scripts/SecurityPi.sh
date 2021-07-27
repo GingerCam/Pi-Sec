ef2aeb013d29a36dbde070550c9049133c489ae5
GingerCam <cameroncoull@gmail.com>
2021-07-27 21:00:05 +0100

    Initial

--- scripts/SecurityPi.sh
+++ scripts/SecurityPi.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
-source /opt/functions.sh
+is_command() {
+    # Checks to see if the given command (passed as a string argument) exists on the system.
+    # The function returns 0 (success) if the command exists, and 1 if it doesn't.
+    local check_command="$1"
 
-if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-    network=online
-else
-    network=offline
-fi
+    command -v "${check_command}" >/dev/null 2>&1
+}
 
-for program in $dependency_file; do
-    command_exists $program
-done
+config_files="dhcpcd.conf dnsmasq.conf hostapd.conf" 
 
-if [[ "$installed" -ne "all" ]]; then
-    install_dependencies
-fi
+setup() {
+    echo "Pi Security written by GingerCam"
+    echo "Applying config"
+    for file in config_files; do 
+    wget https://raw.githubusercontent.com/GingerCam/Pi-0w-security/master/config/$file -o /etc/$file
+    
+    if grep -q "DAEMON_CONF="/etc/hostapd.conf"" "/etc/default/hostapd"; then
+        echo 1
+    else
+        echo ""DAEMON_CONF="/etc/hostapd.conf" >>"/etc/default/hostapd"
+    fi
+    sleep 2
+
+    echo "Setting up wireless location to GB"
+    wpa_cli -i $wireless_interface set country GB
+    wpa_cli -i $wireless_interface save_config >/dev/null 2>&1
+    rfkill unblock wifi
+    for filename in /var/lib/systemd/rfkill*:wlan; do
+        echo 0 >$filename
+    done
+    sleep 1
+    echo "Set"
+    echo "Setting up ssh server"
+    ssh-keygen -A
+    update-rc.d ssh enable
+    invoke-rc.d ssh start
+    echo ""
+    echo "ssh server is configured"
+    echo ""
+}
--- scripts/functions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for file in "functions/"; do
-    source $file
-done
